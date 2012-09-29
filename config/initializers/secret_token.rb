524c78cc590ff14748f7e91b765cd7f100af59ca
Jack <flying_5922@163.com>
2012-09-29 15:00:22 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qdx::Application.config.secret_token = 'b10615f194c324a065926450d60dbc879674bffbf7452fa7466b183c1707ec4370740891d62452250db5c6c51e61d37d757335738947eface34c1c29762ddaad'
