c759d65ae3c3ac3516821d957653cef38ae1e6bb
Jack <flying_5922@163.com>
2012-10-11 17:27:01 +0800

    parse_param finished.

--- app/helpers/product_params_helper.rb
+++ app/helpers/product_params_helper.rb
@@ -1,2 +1,25 @@
 module ProductParamsHelper
+  PARAM_SEP = ':'
+  PARAM_END = ';'
+
+  def parse_param(param_str)
+    param_hash = Hash.new(0)
+
+    error_info = catch(:done) do
+      throw(:done, "param is empty!") if param_str.empty?
+
+      puts "\n\t!!!parse_param begin"
+      puts param_str
+      param_str.split(PARAM_END).each do |param_pair| 
+        tmp = param_pair.split(PARAM_SEP)
+        puts tmp.to_s
+        throw(:done, param_pair) if tmp.size != 2 
+        param_hash[tmp[0]] = tmp[1];
+      end
+      puts "\n\t!!!parse_param end"
+    end
+
+    puts "\n!!! #{error_info}\n" if error_info
+    param_hash
+  end
 end
--- app/models/product_param.rb
+++ app/models/product_param.rb
@@ -1,10 +1,12 @@
 class ProductParam < ActiveRecord::Base
+  include ProductParamsHelper
+
   attr_accessible :param, :product_id
 
   validates :param, length: {maximum: 600}
   validates :product_id, numericality: {only_integer: true, greater_than: 0},
     uniqueness: true
-  validate :validate_product_id
+  validate :validate_product_id, :validate_product_param
 
   belongs_to :product
 
@@ -14,6 +16,15 @@ private
       Product.find(product_id)
     rescue
       errors.add(:product_id, "Invalid product ID!")
+    end
+  end
+
+  def validate_product_param
+    if 0 == param.size
+      errors.add(:param, "param is empty!")
+    end
+    if 0 == parse_param(param).size
+      errors.add(:param, "Invalid param format!")
     end
   end
 end
